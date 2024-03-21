2400a8aae01d51e8b0294b62a27b43ed5fa23de3
ZOUHAYR BAHOUMI <bahoumizouhayr@gmail.com>
2024-03-21 15:06:42 +0000

    ajouter une file js contient les questions et commencer le code js

---
+++ js/questionReponse.js
@@ -0,0 +1,34 @@
+let Questions = [
+    {
+        question : "test 1",
+        reponse : "test reponse",
+        sugguestion : [
+            "choix 1",
+            "choix 2",
+            "choix 3",
+            "choix 4",
+        ]
+    },
+
+    {
+        question : "test 2",
+        reponse : "test reponse 2",
+        sugguestion : [
+            "choix 1",
+            "choix 2",
+            "choix 3",
+            "choix 4",
+        ]
+    },
+
+    {
+        question : "test 3",
+        reponse : "test reponse 3",
+        sugguestion : [
+            "choix 1",
+            "choix 2",
+            "choix 3",
+            "choix 4",
+        ]
+    }
+]
--- js/quiz.js
+++ js/quiz.js
@@ -7,3 +7,15 @@ element.innerHTML='00:'+sec;
 sec++;
 },1000 )
 })()
+
+
+
+
+function afficherQuestions(){
+    let question = document.querySelector(".question");
+    let qstAfficher = '<h2>'+Questions[0].question+'</h2>';
+    question.innerHTML = qstAfficher;
+
+}
+
+afficherQuestions();
