c1a6a65489bccfaefdd8eed32931145812645499
ZOUHAYR BAHOUMI <bahoumizouhayr@gmail.com>
2024-03-21 15:21:18 +0000

    clear js from zouhayr

--- js/quiz.js
+++ js/quiz.js
@@ -1,21 +1,5 @@
-var timer;
-var element= document.getElementsByClassName('.clock');
-(function() {
-var sec= 0;
-timer = setInterval(()=>{
-element.innerHTML='00:'+sec;
-sec++;
-},1000 )
-})()
 
 
 
 
-function afficherQuestions(){
-    let question = document.querySelector(".question");
-    let qstAfficher = '<h2>'+Questions[0].question+'</h2>';
-    question.innerHTML = qstAfficher;
 
-}
-
-afficherQuestions();
